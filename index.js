c91afbf41ef3c8a5f8548f62c716ec2900d8aa4a
andrew65215584 <63922036+andrew65215584@users.noreply.github.com>
2020-11-21 16:27:20 +0200

    start

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 
 
-const contacts = require('./contacts.js');
-const argv = require('yargs').argv;
+// const contacts = require('./contacts.js');
+// const argv = require('yargs').argv;
 
-function invokeAction({ action, id, name, email, phone }) {
-    console.log(action);
+// function invokeAction({ action, id, name, email, phone }) {
+//     console.log(action);
 
     
-    switch (action) {
-        case 'list':
-            contacts.listContacts()
-            break;
-        case 'get':
-            contacts.getContactById(id)
-            break;
-        case 'add':
-            contacts.addContact(name, email, phone)
-            break;
-        case 'remove':
-            contacts.removeContact(id)
-            break;
+//     switch (action) {
+//         case 'list':
+//             contacts.listContacts()
+//             break;
+//         case 'get':
+//             contacts.getContactById(id)
+//             break;
+//         case 'add':
+//             contacts.addContact(name, email, phone)
+//             break;
+//         case 'remove':
+//             contacts.removeContact(id)
+//             break;
         
-        default:
-            console.warn('\x1B[31m Unknown action type!');
-    }
-}
+//         default:
+//             console.warn('\x1B[31m Unknown action type!');
+//     }
+// }
 
-invokeAction(argv);
+// invokeAction(argv);
